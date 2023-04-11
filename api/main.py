8added39396f085f85fbde7bc144e88fd285d5c6
Danie Nell <dnell.personal@gmail.com>
2023-04-12 00:57:22 +0100

    update

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from models import createDB,insertData, readData,updateData
 from modelsV2 import selectTable, selectTable2
 
@@ -15,7 +15,24 @@ app = Flask(__name__)
 def base():
     data = selectTable()
     data2 = selectTable2()
-    return render_template("baseLayout.html",data=data, data2=data2)
+    return render_template("base.html",data=data, data2=data2)
+  
+# Update route
+@app.route('/update', methods=['POST'])
+def update_user():
+    row_id = request.form['id']
+    new_name = request.form['name']
+    new_email = request.form['email']
+
+    # Update the user in the database
+    user = User.query.get(row_id)
+    if row_id:
+        user.name = new_name
+        user.email = new_email
+        db.session.commit()
+        return "User updated successfully"
+    else:
+        return redirect(url_for("base"))
 
 
 if __name__ == '__main__':
