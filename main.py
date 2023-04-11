dfc6f16b65eb6ff0ad63be8bac3d08ec22fe35e0
Danie Nell <dnell.personal@gmail.com>
2023-04-11 14:01:03 +0100

    updates

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from models import createDB,insertData, readData,updateData
-from modelsV2 import selectTable
+from modelsV2 import selectTable, selectTable2
 
 app = Flask(__name__)
 
@@ -14,7 +14,8 @@ app = Flask(__name__)
 @app.route("/")
 def base():
     data = selectTable()
-    return render_template("baseLayout.html",data=data)
+    data2 = selectTable2()
+    return render_template("baseLayout.html",data=data, data2=data2)
 
 
 if __name__ == '__main__':
--- modelsV2.py
+++ modelsV2.py
@@ -16,6 +16,39 @@ host = config.get('database', 'host')
 port = config.get('database', 'port')
 
 
+def get_db_connection():
+    connection = psycopg2.connect(
+        dbname=dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port
+    )
+    cursor = connection.cursor()
+    
+    return connection, cursor
+    # this is in a tuple 0 and 1
+    # if you want to call these you can use 
+    #
+
+
+def selectTable2():
+    with closing(get_db_connection()[0]) as connection:
+        with connection.cursor() as cursor:
+            # Define query
+            query = "SELECT * FROM public.bounties3"
+
+            # Execute query
+            cursor.execute(query)
+
+            # Fetch all rows from the result
+            rows = cursor.fetchall()
+
+            print(rows)
+
+            # Return the fetched rows
+            return rows
+
 def selectTable():
     # Connect to the database using psycopg2
     with closing(psycopg2.connect(
@@ -27,7 +60,7 @@ def selectTable():
     )) as connection:
         with connection.cursor() as cursor:
             # Define query
-            query = "SELECT * FROM bounties"
+            query = "SELECT * FROM public.bounties3"
 
             # Execute query
             cursor.execute(query)
