bcecfa80d8a50fe6e3eb5e61fe107c449b095067
Danie Nell <dnell.personal@gmail.com>
2023-04-16 20:26:28 +0100

    ..

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
-import sys
-from pathlib import Path
+# import sys
+# from pathlib import Path
 
-sys.path.append(str(Path(__file__).parent))
+# sys.path.append(str(Path(__file__).parent))
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
--- api/config.py
+++ api/config.py
@@ -2,7 +2,7 @@ import configparser
 
 def load_config():
     config = configparser.ConfigParser()
-    config.read('api/configs.ini')
+    config.read('configs.ini')
 
     db_uri = f"postgresql://{config.get('database', 'user')}:{config.get('database', 'password')}@{config.get('database', 'host')}:{config.get('database', 'port')}/{config.get('database', 'dbname')}"
     return db_uri
