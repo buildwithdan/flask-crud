a9d961babd8b97025e18b234b9e8a2ae19eef43e
Danie Nell <dnell.personal@gmail.com>
2023-04-16 19:41:44 +0100

    ..

--- api/app.py
+++ api/app.py
@@ -8,18 +8,21 @@ from flask_sqlalchemy import SQLAlchemy
 from config import load_config
 
 app = Flask(__name__)
+
+
 # application = app #for the WSGI service
 app.config['SQLALCHEMY_DATABASE_URI'] = load_config()
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# Create the tables if they don't exist
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 import views
+
+
+# Create the tables if they don't exist
+
+
+
 
 if __name__ == "__main__": 
     app.run()
